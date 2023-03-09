c934e58afd3f6210363a2bcf0e374de43f495d8d
LENOVO <LENOVO@shivajighuge>
2023-03-09 16:10:24 +0530

    push msg

--- src/sampleProject/SampleClass1.java
+++ src/sampleProject/SampleClass1.java
@@ -4,7 +4,9 @@ public class SampleClass1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hii");
+		System.out.println("Hii");
+		System.out.println("Hii hello rahul");
+
 	}
 
 }
